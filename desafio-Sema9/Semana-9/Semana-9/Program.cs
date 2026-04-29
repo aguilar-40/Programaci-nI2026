ab25f16e5f0b74055655301f41d151d40ad62c61
ayllin <aylinjuditha@gmail.com>
2026-04-29 14:30:23 -0600

    desafio-Sema9

---
+++ desafio-Sema9/Semana-9/Semana-9/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System;
+using System.Text.RegularExpressions;
+
+class Program {
+    static void Main() {
+        string texto = "Este es mi correo electrónico: juan_perez.23@gmail.com. Por favor, envíeme un correo con sus comentarios.";
+        
+        string patron = @"[a-zA-Z0-9._-]+@[a-zA-Z.]+\.[a-z]{2,}";
+
+        MatchCollection correos = Regex.Matches(texto, patron);
+
+        foreach (Match correo in correos) {
+            Console.WriteLine(correo.Value);
+        }
+    }
+}
