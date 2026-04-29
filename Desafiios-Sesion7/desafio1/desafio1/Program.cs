6774f573cc3ec880dbc35b9d2313e57aeaeca4a0
ayllin <aylinjuditha@gmail.com>
2026-04-29 13:35:47 -0600

    Desafiios-Sesion7

---
+++ Desafiios-Sesion7/desafio1/desafio1/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+class Calculadora {
+    public string Marca;
+    public string Serie;
+
+    public int Sumar(int a, int b) => a + b;
+    public int Restar(int a, int b) => a - b;
+    public int Multiplicar(int a, int b) => a * b;
+    public double Dividir(int a, int b) => (double)a / b;
+}
+
+class CalculadoraCientifica : Calculadora {
+    public double Potencia(double a, double b) => Math.Pow(a, b);
+    public double Raiz(double a) => Math.Sqrt(a);
+    public int Modulo(int a, int b) => a % b;
+    public double Logaritmo(double a) => Math.Log(a);
+}
+
+class Program {
+    static void Main() {
+        Calculadora calc = new Calculadora();
+        Console.WriteLine("Suma: " + calc.Sumar(5, 3));
+
+        CalculadoraCientifica cient = new CalculadoraCientifica();
+        Console.WriteLine("Potencia: " + cient.Potencia(2, 4));
+    }
+}
---
+++ Desafiios-Sesion7/desafio1/desafio2/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+interface INotificable {
+    void Notifica(string mensaje);
+}
+
+class NotificacionEmail : INotificable {
+    public string direccionCorreo;
+    public void Notifica(string mensaje) {
+        Console.WriteLine($"Email a {direccionCorreo}: {mensaje}");
+    }
+}
+
+class NotificacionWhatsap : INotificable {
+    public string numeroTelefono;
+    public void Notifica(string mensaje) {
+        Console.WriteLine($"WhatsApp a {numeroTelefono}: {mensaje}");
+    }
+}
+
+class NotificacionSMS : INotificable {
+    public string numeroTelefono;
+    public void Notifica(string mensaje) {
+        Console.WriteLine($"SMS a {numeroTelefono}: {mensaje}");
+    }
+}
+
+class Program {
+    static void Main() {
+        NotificacionEmail email = new NotificacionEmail { direccionCorreo = "correo@ejemplo.com" };
+        email.Notifica("Hola por Email!");
+
+        NotificacionWhatsap whatsapp = new NotificacionWhatsap { numeroTelefono = "555-1234" };
+        whatsapp.Notifica("Hola por WhatsApp!");
+
+        NotificacionSMS sms = new NotificacionSMS { numeroTelefono = "555-5678" };
+        sms.Notifica("Hola por SMS!");
+    }
+}
